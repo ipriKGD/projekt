46d6438e0c32d4b53697b1abd3ebedef5b267a62
km4054 <km4054@student.uni-lj.si>
2014-04-16 15:01:54 +0200

    preverjanje za user.online

--- app/js/app.js
+++ app/js/app.js
@@ -8,10 +8,14 @@ var myApp = angular.module('myApp', [
   'socialTradeServices',
   'myApp.directives',
   'socialTradeControllers',
-  'socialTradeAnimations'
+  'socialTradeAnimations',
+
 ]);
 myApp.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/trades', {templateUrl: 'partials/article-list.html', controller: 'ArticleListCtrl'});
+  $routeProvider.when('/trades', {templateUrl: 'partials/article-list.html', controller: 'ArticleListCtrl' });
   $routeProvider.when('/trades/:tradeId', {templateUrl: 'partials/article-detail.html', controller: 'ArticleDetailCtrl'});
+  $routeProvider.when('/login', {templateUrl: "partials/login.html" });
+  $routeProvider.when('/logout',  {templateUrl: "partials/logout.html"});
+  $routeProvider.when("/signup", {templateUrl: "partials/signup.html", });
   $routeProvider.otherwise({redirectTo: '/trades'});
 }]);
