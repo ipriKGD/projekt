4119c6ac3dbe6f19911c3077e8647da0a8465b8c
km4054 <km4054@student.uni-lj.si>
2014-04-18 00:43:16 +0200

    za heroku

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/app'));
 var port = process.env.PORT || 8000;
 app.listen(port, function(){
 console.log('listening on', port);
