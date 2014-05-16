83057c3b105f7c7dff0c10ae4a73a0453d2ff26d
km4054 <km4054@student.uni-lj.si>
2014-05-16 18:41:05 +0200

    Retrieving trades form Firebase

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,23 +4,25 @@
 /* Controllers */
 var socialTradeControllers = angular.module('socialTradeControllers', []);
 
-socialTradeControllers.controller('ArticleListCtrl', ['$scope', 'socialTradeService',
-  function($scope, socialTradeService) {
-    $scope.trades = socialTradeService.trades.query();
+socialTradeControllers.controller('ArticleListCtrl', ['$scope', 'socialTradeService', 'syncData', 
+  function($scope, socialTradeService, syncData) {
+    //$scope.trades = socialTradeService.trades.query();
+    $scope.trades = syncData('trades');
     $scope.order = '-created';
     $scope.category_show = 'all';
     $scope.showfilter = "1";
-    // TODO: preveri datume in avtomatsko zapri potecene trade (v backendu ali filtru)
 
   }]);
 
-socialTradeControllers.controller('ArticleDetailCtrl', ['$scope', '$routeParams', 'socialTradeService',
-  function($scope, $routeParams, socialTradeService) {
-    $scope.trade = socialTradeService.trades.get({tradeId: $routeParams.tradeId}, function(trade) {
-    	$scope.mainImage = trade.article.image[0];
+socialTradeControllers.controller('ArticleDetailCtrl', ['$scope', '$routeParams', 'socialTradeService', 'syncData',
+  function($scope, $routeParams, socialTradeService, syncData) {
+   //$scope.trade = socialTradeService.trades.get({tradeId: $routeParams.tradeId}, function(trade) {
+   // console.log('trades/'+$routeParams.tradeId);
+    $scope.trade = syncData(['trades', $routeParams.tradeId]);
+    $scope.mainImage = $scope.trade.article.image[0];
     	//get needed values from trade object
 
-    });
+    
     
     $scope.setImage = function(image) {
       $scope.mainImage = image;
@@ -29,24 +31,29 @@ socialTradeControllers.controller('ArticleDetailCtrl', ['$scope', '$routeParams'
     	// TODO: PUT zahtevek za spremembo vrednosti atributa active v 0
     }
 
-    $scope.$watch('trade', function(oldValue, newValue) {
+    /*$scope.$watch('trade', function(oldValue, newValue) {
 		if (!newValue) {
 			$scope.tradeArticle = null;
 			$scope.tradeUser = null;
 			return;
 		}
 		newValue.$promise.then(function() {
-			$scope.tradeArticle = socialTradeService.articles.get({
+			/*$scope.tradeArticle = socialTradeService.articles.get({
 				articleId : $scope.trade.article_id
 			});
+      $scope.tradeArticle = syncData(['articles', $scope.trade.article_id]);
 		});
 		newValue.$promise.then(function() {
-			$scope.tradeUser = socialTradeService.users.get({
+			/*$scope.tradeUser = socialTradeService.users.get({
 				userId : $scope.trade.user_id
-			});
-		});
-		
-	});
+			}); 
+      $scope.tradeUser = syncData(['users', $scope.trade.user_id]);
+		}); */
+    $scope.tradeArticle = $scope.tradeArticle = syncData(['articles', $scope.trade.article_id]);
+    $scope.tradeUser = syncData(['users', $scope.trade.user_id]);
+		//bind trade pomeni, da dodas tisto v userja/article!!! PAZI!!
+    //bind user bi verjetno pomenil update!!! (torej daj bind na trade!!! !)
+	
 
 
 	
@@ -83,13 +90,16 @@ socialTradeControllers.controller('UserEditCtrl', ['$scope', 'socialTradeService
 /*V VSAK CONTROLLER JE POTREBNO INJECTAT $firebase service
  firebase lahko naredimo kot service - glej angularfire seed project
  ALI v kontrolerju: 
-          var ref = new Firebase("https://feg3jk6f9bz.firebaseio-demo.com/");
-          $scope.messages = $firebase(ref);
+          var messagesRef = new Firebase("https://feg3jk6f9bz.firebaseio-demo.com/");
+          $scope.messages = $firebase(messagesRef);
           //dodajanje
           $scope.addMessage = function(e) { //to je event na input submit ng-keydown="addmessage(...)"
             if (e.keyCode != 13) return;
             $scope.messages.$add({from: $scope.name, body: $scope.msg});
             $scope.msg = "";
+            //modify data: messagesRef.update({name:'alex', age:35}) ali $add, $remove, $update
+
+            //kako deluje select? - morda kar v url dodas id? (sej itak za to imamo filterje!!!)
           };
 
 */
